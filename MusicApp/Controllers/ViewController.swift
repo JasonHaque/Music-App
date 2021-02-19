0a53d3b4c382690d53f2c7e993fdd964def8a7f4
JasonHaque <zahinjason220434@gmail.com>
2021-02-19 19:21:57 +0600

    code added to remove storyboards

--- MusicApp/Controllers/ViewController.swift
+++ MusicApp/Controllers/ViewController.swift
@@ -11,7 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Home"
+        view.backgroundColor = .red
+        
     }
 
 
--- MusicApp/Resources/AppDelegate.swift
+++ MusicApp/Resources/AppDelegate.swift
@@ -11,9 +11,15 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    var window : UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        
         return true
     }
 
