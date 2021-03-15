e7d60b7058f2e4a68a6ed9b85862a3720f0fc857
JasonHaque <zahinjason220434@gmail.com>
2021-03-15 19:52:59 +0600

    base code added to api caller

--- MusicApp/Managers/APICaller.swift
+++ MusicApp/Managers/APICaller.swift
@@ -14,11 +14,43 @@ final class APICaller{
     private init(){}
     
     public func getCurrentUserProfile(completion : @escaping (Result<UserProfile,Error>) -> Void){
-        AuthManager.shared.withValidToken { token in
-            
-            //code to be done
+        
+        createRequest(with: URL(string: ""), type: .GET) { baseRequest in
             
         }
+        
+    }
+    
+    enum HTTPMethod : String{
+        
+        case GET
+        case POST
+        
+    }
+    
+    private func createRequest(with url : URL?,type : HTTPMethod,completion : @escaping (URLRequest) -> Void) {
+        
+        AuthManager.shared.withValidToken { token in
+            
+            guard let apiURL = url else{
+                return
+            }
+            
+            //code to be done
+            var request = URLRequest(url: apiURL)
+            
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+            
+            request.httpMethod = type.rawValue
+            
+            request.timeoutInterval = 30
+            
+            completion(request)
+            
+        }
+        
+        
+        
     }
     
 }
