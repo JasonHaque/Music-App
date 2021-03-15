40aa7c97400b58b8d375fd9e6869719593759371
JasonHaque <zahinjason220434@gmail.com>
2021-03-15 20:21:06 +0600

    data fetch successful

--- MusicApp/Controllers/Core/HomeViewController.swift
+++ MusicApp/Controllers/Core/HomeViewController.swift
@@ -14,6 +14,16 @@ class HomeViewController: UIViewController {
         
         view.backgroundColor = .systemBackground
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTapSettings))
+        
+    }
+    
+    
+    @objc private func didTapSettings(){
+        let vc = ProfileViewController()
+        vc.title = "Profile"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
     }
 
 
