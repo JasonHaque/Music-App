2406d6d4347ecf8f5ad92c11beb6ace26a96ccd0
JasonHaque <zahinjason220434@gmail.com>
2021-03-16 02:11:27 +0600

    added customizations to profile

--- MusicApp/Controllers/Others/ProfileViewController.swift
+++ MusicApp/Controllers/Others/ProfileViewController.swift
@@ -7,26 +7,56 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    private let tableView : UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Profile"
+        fetchProfile()
+        view.backgroundColor = .systemBackground
         
-        APICaller.shared.getCurrentUserProfile { result in
+    }
+    
+    
+    private func fetchProfile(){
+        APICaller.shared.getCurrentUserProfile {[weak self] result in
             
-            switch result{
-            
-            case .success(let model):
-                break
-            case .failure(let error):
-                print(error.localizedDescription)
+            DispatchQueue.main.async {
+                switch result{
+
+                case .success(let model):
+                    self?.updateUI(with : model)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self?.failedToGetProfile()
+                }
+                
             }
             
         }
     }
     
+    private func updateUI(with model : UserProfile){
+        
+    }
+    
+    private func failedToGetProfile(){
+        let label = UILabel(frame: .zero)
+        
+        label.text = "Failed to load Profile"
+        label.textColor = .secondaryLabel
+        label.sizeToFit()
+        label.center = view.center
+        
+        view.addSubview(label)
+    }
 
     
 
