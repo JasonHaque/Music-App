414c985255278ecdaf9a77161f438ebb15d69666
JasonHaque <zahinjason220434@gmail.com>
2021-03-15 23:00:30 +0600

    user profile model added

--- MusicApp/Models/UserProfile.swift
+++ MusicApp/Models/UserProfile.swift
@@ -9,4 +9,22 @@ import Foundation
 
 struct UserProfile{
     
+    let country : String
+    let display_name : String
+    let email : String
+    let explicit_content : [String : Int]
+    let external_urls : [String : String]
+    let followers : [String : Codable?]
+    let id : String
+    let product : String
+    let images : [UserImage]
 }
+
+struct UserImage :Codable {
+    let url : String
+}
+
+
+
+
+
