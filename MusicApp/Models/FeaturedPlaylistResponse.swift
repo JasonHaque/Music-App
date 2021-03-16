a7c974afa8c5ce71ea1bdb77d16af8c68010ca40
JasonHaque <zahinjason220434@gmail.com>
2021-03-17 02:59:41 +0600

    models written for api response

---
+++ MusicApp/Models/FeaturedPlaylistResponse.swift
@@ -0,0 +1,32 @@
+//
+//  FeaturedPlaylistResponse.swift
+//  MusicApp
+//
+//  Created by Sanviraj Zahin Haque on 17/3/21.
+//
+
+import Foundation
+
+struct FeaturedPlaylistResponse : Codable{
+    let playlists : [PlaylistResponse]
+}
+
+struct PlaylistResponse : Codable{
+    
+    let items : [Playlist]
+}
+
+struct Playlist: Codable{
+    let description : String
+    let external_urls :[String : String]
+    let id : String
+    let images : [APIImage]
+    let name : String
+    let owner : User
+}
+
+struct User: Codable{
+    let display_name : String
+    let external_urls :[String : String]
+    let id : String
+}
