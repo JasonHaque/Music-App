f3875c14f313bde1f746e75508cf2fbab45f112f
JasonHaque <zahinjason220434@gmail.com>
2021-03-22 23:04:44 +0600

    new view models added

---
+++ MusicApp/ViewModels/FeaturedPlayListCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  FeaturedPlayListCellViewModel.swift
+//  MusicApp
+//
+//  Created by Sanviraj Zahin Haque on 22/3/21.
+//
+
+import Foundation
+
+struct FeaturedPlayListCellViewModel{
+    
+    let name : String
+    let artWorkURL : URL?
+    let creatorName : String
+}
---
+++ MusicApp/ViewModels/RecommendedTrackCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RecommendedTrackViewModel.swift
+//  MusicApp
+//
+//  Created by Sanviraj Zahin Haque on 22/3/21.
+//
+
+import Foundation
+
+struct RecommendedTrackCellViewModel{
+    let name : String
+    let artistName : String
+    let artWorkURL : URL?
+}
