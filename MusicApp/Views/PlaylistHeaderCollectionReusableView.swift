daeac55c928e3395d905fcf61c62aa78dcac6799
JasonHaque <zahinjason220434@gmail.com>
2021-04-22 20:50:53 +0600

    header done for playlist controller

--- MusicApp/Views/PlaylistHeaderCollectionReusableView.swift
+++ MusicApp/Views/PlaylistHeaderCollectionReusableView.swift
@@ -20,12 +20,15 @@ final class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
     
     private let descriptionLabel : UILabel = {
         let label = UILabel()
+        label.textColor = .secondaryLabel
         label.font = .systemFont(ofSize: 18, weight : .regular)
+        label.numberOfLines = 0
         return label
     }()
     
     private let ownerLabel : UILabel = {
         let label = UILabel()
+        label.textColor = .secondaryLabel
         label.font = .systemFont(ofSize: 18, weight : .light)
         return label
     }()
@@ -40,7 +43,7 @@ final class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
     //MARK:- init
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .red
+        backgroundColor = .systemBackground
         addSubview(imageView)
         addSubview(nameLabel)
         addSubview(descriptionLabel)
@@ -53,6 +56,13 @@ final class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        
+        let imageSize = height/1.8
+        
+        imageView.frame = CGRect(x: (width-imageSize)/2, y: 20, width: imageSize, height: imageSize)
+        nameLabel.frame = CGRect(x: 10, y: imageView.bottom, width: width-20, height: 44)
+        descriptionLabel.frame = CGRect(x: 10, y: nameLabel.bottom, width: width-20, height: 44)
+        ownerLabel.frame = CGRect(x: 10, y: descriptionLabel.bottom, width: width-20, height: 44)
     }
     
     func configure(with viewModel : PlayListHeaderViewModel){
