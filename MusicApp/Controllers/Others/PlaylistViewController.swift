4b96178deaa9c333f312c88c91770f3806aa27e7
JasonHaque <zahinjason220434@gmail.com>
2021-04-22 13:47:43 +0600

    code

--- MusicApp/Controllers/Others/PlaylistViewController.swift
+++ MusicApp/Controllers/Others/PlaylistViewController.swift
@@ -11,6 +11,21 @@ class PlaylistViewController: UIViewController {
 
     private let playlist : Playlist
     
+    private let collectionView : UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: { _, _  -> NSCollectionLayoutSection? in
+        
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0)))
+        
+        item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+        
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(80)), subitem: item, count: 1)
+        
+       
+        
+        let section = NSCollectionLayoutSection(group: group)
+       
+        return section
+    }))
+    
     init(playlist : Playlist){
         self.playlist = playlist
         super.init(nibName: nil, bundle: nil)
@@ -20,24 +35,63 @@ class PlaylistViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private var viewModels = [RecommendedTrackCellViewModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(RecommendedTracksCollectionViewCell.self, forCellWithReuseIdentifier: RecommendedTracksCollectionViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
 
         title = playlist.name
         view.backgroundColor = .systemBackground
         
-        APICaller.shared.getPlayListDetails(for: playlist) { result in
+        APICaller.shared.getPlayListDetails(for: playlist) {[weak self] result in
             
             DispatchQueue.main.async {
-                switch result{
-                
-                case .success(let model):
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription)
+                DispatchQueue.main.async {
+                    switch result{
+                    
+                    case .success(let model):
+                        // create view models
+                        break
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
                 }
             }
             
         }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        collectionView.frame = view.bounds
+    }
+}
+
+extension PlaylistViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecommendedTracksCollectionViewCell.identifier, for: indexPath) as? RecommendedTracksCollectionViewCell else{
+            return UICollectionViewCell()
+        }
+        cell.backgroundColor = .red
+        //cell.configure(with: viewModels[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
     }
 }
