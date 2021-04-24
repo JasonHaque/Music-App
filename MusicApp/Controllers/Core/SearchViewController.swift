c4b55e3ea815d2250aaeb8ec08271e3ea2604418
JasonHaque <zahinjason220434@gmail.com>
2021-04-24 08:55:58 +0600

    primary search resukt controller added

--- MusicApp/Controllers/Core/SearchViewController.swift
+++ MusicApp/Controllers/Core/SearchViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchResultsUpdating {
+    
+    let searchController : UISearchController = {
+        let result = UIViewController()
+        result.view.backgroundColor = .red
+        let vc = UISearchController(searchResultsController: result)
+        vc.searchBar.placeholder = "SONGS , ARTISTS , ALBUMS"
+        vc.searchBar.searchBarStyle = .minimal
+        vc.definesPresentationContext = true
+        return vc
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
     }
+    
+    
+    //MARK:- search controller
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text, !query.trimmingCharacters(in: .whitespaces).isEmpty else{
+            
+            return
+        }
+        
+        print(query)
+        
+        //perform the search
+    }
+    
+    
     
 
    
