98db44193eb223917304f7ed6d0dc32906bc54d2
JasonHaque <zahinjason220434@gmail.com>
2021-04-25 16:00:46 +0600

    tableview added

--- MusicApp/Controllers/Others/SearchResultsViewController.swift
+++ MusicApp/Controllers/Others/SearchResultsViewController.swift
@@ -10,19 +10,51 @@ import UIKit
 class SearchResultsViewController: UIViewController {
 
     private var results : [SingleSearchResult] = []
+    
+    private let tableView : UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.isHidden = true
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .clear
+        
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
 
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
 
     func update(with results : [SingleSearchResult]){
         self.results = results
+        tableView.reloadData()
+        
+        tableView.isHidden = results.isEmpty
     }
 
+}
+
+extension SearchResultsViewController : UITableViewDelegate , UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "FOO"
+        return cell
+        
+    }
+    
+    
 }
