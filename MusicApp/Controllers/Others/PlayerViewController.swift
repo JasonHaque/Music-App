eba1d7134b98c77a7fd1f4fbbb2cf9569492f5ae
JasonHaque <zahinjason220434@gmail.com>
2021-04-26 18:30:08 +0600

    player view changed

--- MusicApp/Controllers/Others/PlayerViewController.swift
+++ MusicApp/Controllers/Others/PlayerViewController.swift
@@ -8,15 +8,40 @@
 import UIKit
 
 class PlayerViewController: UIViewController {
+    
+    private let imageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemBlue
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBackground
+        
+        view.addSubview(imageView)
+        configureBarButtonItems()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.width, height: view.width)
+    }
 
+    private func configureBarButtonItems(){
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(didTapClose))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapAction))
+    }
     
+    @objc private func didTapClose(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc private func didTapAction(){
+        
+    }
 
 }
--- MusicApp/Presenter/PlaybackPresenter.swift
+++ MusicApp/Presenter/PlaybackPresenter.swift
@@ -12,15 +12,15 @@ final class PlaybackPresenter{
     
     static func startPlayback(from viewController : UIViewController , track : AudioTrack){
         let vc = PlayerViewController()
-        
-        viewController.present(vc, animated: true, completion: nil)
+        vc.title = track.name
+        viewController.present(UINavigationController(rootViewController: vc), animated: true, completion: nil)
         
     }
     
     static func startPlayback(from viewController : UIViewController , tracks : [AudioTrack]){
         let vc = PlayerViewController()
         
-        viewController.present(vc, animated: true, completion: nil)
+        viewController.present(UINavigationController(rootViewController: vc), animated: true, completion: nil)
     }
     
     
